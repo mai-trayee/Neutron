cac45688cd29117a9fec88c86bbccb2c73448136
Tommaso.Boschi <tboschi@heppc401.ph.qmul.ac.uk>
2020-02-13 15:11:32 +0000

    headers

--- Makefile
+++ Makefile
@@ -17,35 +17,92 @@ LDFLAGS  :=	-Wl,--no-as-needed $(LDFLAGS) $(ROOTLIB) $(G4LIB) -L$(LIBDIR)
 CXXFLAGS :=	$(CXXFLAGS) -O3 -mavx $(ROOTCXX) $(G4CXX) -I$(INCDIR)
 
 #exectuables
-CPP =	CfInScint \
-	ReadRoot
-	#First	\
+cscin =	CfInScint	\
+
+ctrig = CfTrigger	\
+	VisTrigger
+
+csk = visSK
+
+cother = VMEread	\
+	 Unify
+	#ReadRoot	\
+	ExtractRoot	
 
 #header folders
-HPP =	CfInScint/MsgOutput	\
-	CfInScint/MsgVolume	\
-	CfInScint/Volume	\
-	CfInScint/Run		\
-	CfInScint/Generator	\
-	CfInScint/Event		\
-	CfInScint/Step		\
-	#First	\
+hscin =	InScint/InScintMsgOutput	\
+	InScint/InScintMsgVolume	\
+	InScint/InScintVolume		\
+	InScint/InScintRun		\
+	InScint/InScintGenerator	\
+	InScint/InScintEvent		\
+	InScint/InScintStep		
+
+htrig = Trigger/TriggerMsgOutput	\
+	Trigger/TriggerMsgVolume	\
+	Trigger/TriggerVolume		\
+	Trigger/TriggerRun		\
+	Trigger/TriggerGenerator	\
+	Trigger/TriggerEvent		\
+	Trigger/TriggerStep		\
+
+hsk = SK/WCSimDarkRateMessenger.hh	\
+      SK/WCSimEnumerations.hh		\
+      SK/WCSimRootOptions.hh       	\
+      SK/WCSimWCDigi.hh	\
+      SK/WCSimDetectorConstruction.hh  	\
+      SK/WCSimTuningMessenger.hh   	\
+      SK/WCSimWCHit.hh	\
+      SK/WCSimDetectorMessenger.hh     	\
+      SK/WCSimTuningParameters.hh  	\
+      SK/WCSimWCPMT.hh	\
+      SK/WCSimPmtInfo.hh               	\
+      SK/WCSimVisManager.hh	\
+      SK/WCSimPMTObject.hh             	\
+      SK/WCSimWCAddDarkNoise.hh	\
+
+hother = LoadTrack			\
+	 PMT2root			\
+	 Unify
 
 #main targets
-TGT :=	$(CPP:%=$(APPDIR)/%)
+Tscin := $(cscin:%=$(APPDIR)/%)
+Ttrig := $(ctrig:%=$(APPDIR)/%)
+Tsk   := $(csk:%=$(APPDIR)/%)
+Tother := $(cother:%=$(APPDIR)/%)
 
 #dependencies of targers
 #INCDEP :=	$(HPP:%=$(INCDIR)/%/*.cpp)
-INCDEP := 	$(HPP:%=$(INCDIR)/%.cpp)
-DEP:=		$(patsubst %.cpp,%.o,$(wildcard $(INCDEP)))
+Iscin := $(hscin:%=$(INCDIR)/%.cpp)
+Itrig := $(htrig:%=$(INCDIR)/%.cpp)
+Isk   := $(hsk:%=$(INCDIR)/%.cpp)
+Iother := $(hother:%=$(INCDIR)/%.cpp)
 
-all:	$(TGT)
+Dscin := $(patsubst %.cpp,%.o,$(wildcard $(Iscin)))
+Dtrig := $(patsubst %.cpp,%.o,$(wildcard $(Itrig)))
+Dsk   := $(patsubst %.cpp,%.o,$(wildcard $(Isk)))
+Dother := $(patsubst %.cpp,%.o,$(wildcard $(Iother)))
+
+inscint: $(Tscin) all
+	-@mv $(Tscin) $(BINDIR)
+
+trigger: $(Ttrig) all
+	-@mv $(Ttrig) $(BINDIR)
+
+sk : $(Tsk) all
+	-@mv $(Tsk) $(BINDIR)
+
+other: $(Tother) all
+	-@mv $(Tother) $(BINDIR)
+
+all:
 	@mkdir -p $(BINDIR)
 	@mkdir -p $(LIBDIR)
 	@echo "Moving stuff..."
-	@mv $(TGT) $(BINDIR)
 
-$(TGT): $(DEP)
+$(Tscin): $(Dscin)
+$(Ttrig): $(Dtrig)
+$(Tother): $(Dother)
 
 clean:
 	find $(INCDIR) -mindepth 1 -name "*.o" -delete
--- app/CfInScint.cpp
+++ app/CfInScint.cpp
@@ -1,6 +1,7 @@
 //std
 
-#include <string>
+#include <cstring>
+#include <getopt.h>
 
 //Geant4
 #include "G4ios.hh"
@@ -10,14 +11,76 @@
 #include "FTFP_BERT_HP.hh"
 
 //user defined
-#include "CfInScint/Volume.h"
-#include "CfInScint/Generator.h"
-#include "CfInScint/Run.h"
-#include "CfInScint/Event.h"
-#include "CfInScint/Step.h"
+#include "InScint/InScintVolume.h"
+#include "InScint/InScintGenerator.h"
+#include "InScint/InScintRun.h"
+#include "InScint/InScintEvent.h"
+#include "InScint/InScintStep.h"
 
+void Usage(char *argv0);
 int main(int argc, char** argv)
 {
+	const struct option longopts[] = 
+	{
+		{"macro", 	required_argument,	0, 'm'},
+		{"dimension", 	required_argument,	0, 'L'},
+		{"cube", 	no_argument,		0, 'q'},
+		{"cylinder", 	no_argument,		0, 'c'},
+		{"plastic", 	no_argument,		0, 'P'},
+		{"bgo", 	no_argument,		0, 'B'},
+		{"output", 	required_argument,	0, 'o'},
+		{"help", 	no_argument,	 	0, 'h'},
+		{0,	0, 	0,	0},
+	};
+
+	int index; 
+	int iarg = 0;
+	opterr = 1;
+	
+	//Initialize variables
+	std::string outName, macFile;
+	double size = 5;	//5 cm
+	bool cube = true;	//cylinder = false
+	bool plastic = true;	//bgo      = false
+	unsigned int numEvents = 1000;
+	
+	while((iarg = getopt_long(argc,argv, "m:L:qcPBo:n:h", longopts, &index)) != -1)
+	{
+		switch(iarg)
+		{
+			case 'm':
+				macFile.assign(optarg);
+				break;
+			case 'L':
+				size = std::strtod(optarg, NULL);
+				break;
+			case 'q':
+				cube = true;
+				break;
+			case 'c':
+				cube = false;
+				break;
+			case 'P':
+				plastic = true;
+				break;
+			case 'B':
+				plastic = false;
+				break;
+			case 'o':
+				outName.assign(optarg);
+				break;
+			case 'n':
+				numEvents = std::strtol(optarg, NULL, 10);
+				break;
+			case 'h':
+				Usage(argv[1]);
+				break;
+			default:
+				return 1;
+				break;
+		}
+	}
+
 	// construct the default run manager
 	G4RunManager* runManager = new G4RunManager;
 	
@@ -42,12 +105,53 @@ int main(int argc, char** argv)
 	// initialize Gu kernel
 	runManager->Initialize();
 
-	G4String command = "/control/execute ";
-	G4String Batch = argv[1];
-	G4cout << "Running macro " << Batch << std::endl;
-	UI->ApplyCommand(command+Batch);
+	UI->ApplyCommand("/control/execute " + macFile);
+
+	//outName = "/Output/RootFile " + outName;
+	G4String dim, shape, type;
+
+	std::stringstream sSize;
+	sSize << size;
+	sSize >> dim;
+
+	if (cube)
+		shape = "Cube";
+	else
+		shape = "Cylinder";
+
+	if (plastic)
+		type = "Plastic";
+	else
+		type = "BGO";
+
+	outName += "_" + shape + "_" + type + "_" + dim + ".root";
+
+	UI->ApplyCommand("/Output/RootFile " + outName);
+	UI->ApplyCommand("/Volume/Size " + dim + " cm");
+	UI->ApplyCommand("/Volume/Shape " + shape);
+	UI->ApplyCommand("/Volume/Type " + type);
+	UI->ApplyCommand("/Volume/Construct");
+
+	runManager->BeamOn(numEvents);
 
 	//job termination
 	delete runManager;
 	return 0;
+}
+
+void Usage(char* argv0)
+{
+	std::cout << "Description" << std::endl;
+	std::cout << "Usage : " << std::endl;
+	std::cout << argv0 << " [OPTIONS]" << std::endl;
+	std::cout <<"\n  -i,  --input" << std::endl;
+	std::cout << "\t\tInput file, tabulated as Mass\tUu\tnEvt" << std::endl;
+	std::cout <<"\n  -o,  --output" << std::endl;
+	std::cout << "\t\tOutput file" << std::endl;
+	std::cout <<"\n  -t,  --threshold" << std::endl;
+	std::cout << "\t\tEvent threshold for signal. As mass dependent threshold, t is the y-intercept" << std::endl;
+	std::cout <<"\n  -m,  --massdep" << std::endl;
+	std::cout << "\t\tIf specified, a mass dependance is considered for threshold, [thr] = t + m * [Mass]" << std::endl;
+	std::cout <<"\n  -h,  --help" << std::endl;
+	std::cout << "\t\tPrint this message and exit" << std::endl;
 }
